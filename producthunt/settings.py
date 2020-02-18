02d7eac0ad905038252fd7a2eb3a29dcf615d8ef
Utkarsh srivastava <utak6543@gmail.com>
2020-02-18 22:15:18 +0530

    my fist commit

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -126,3 +126,5 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'producthunt/static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=30)
+    url = models.CharField(max_length=30)
+    pub_date = models.DateTimeField()
+    body = models.CharField(max_length=200)
+    image = models.ImageField(upload_to = 'images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default = 1)
+    hunter = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body [:70]
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%c')
